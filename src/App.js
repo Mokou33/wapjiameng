46ee16ab5e19ff40dfc6064a3d8ae4503cc8bd14
scarlet-moon <978542146@qq.com>
2019-04-18 14:19:13 +0800

    start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+
+import './comment/css/reset.css'
+
+import Layout from "./components/layout";
+import {routers} from "./routers";
+
+
+
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Router>
+          <Layout>
+            <Switch>
+              {
+                routers.map((item, index) => {
+                  return (
+                      <Route
+                          key={index}
+                          exact={item.exact}
+                          path={item.path}
+                          component={item.component}
+                      ></Route>
+                  )
+                })
+              }
+            </Switch>
+          </Layout>
+        </Router>
     );
   }
 }
